bf6fe268e7efcc64f734aeb7a33284f966ce9b4c
KalpitRao <kalpitraorane46@gmail.com>
2023-01-23 14:01:48 +0530

    Initial commit

--- src/Hooks/UseStateObject.js
+++ src/Hooks/UseStateObject.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 function UseStateObject() {
   return (
-    <div>UseStateObject</div>
+    <>
+    <div>
+        <h1>
+            Name:"kalpit" ,Age:27, Degree: BscIt
+        </h1>
+        <button>
+            Update
+        </button>
+
+
+
+        </div>
+    </>
+    
   )
 }
 
