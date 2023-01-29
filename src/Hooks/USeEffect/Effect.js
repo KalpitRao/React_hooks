2d9a45b809048663dd801ca7b1eeb402f521b1bb
KalpitRao <kalpitraorane46@gmail.com>
2023-01-29 13:50:34 +0530

    Initial commit

--- src/Hooks/USeEffect/Effect.js
+++ src/Hooks/USeEffect/Effect.js
@@ -1,22 +1,40 @@
 import React, { useEffect, useState } from 'react'
 
 function Effect() {
-    const [count, setcount] = useState(0)
+  const [count, setcount] = useState(0)
 
+  //Try every use effect
+  useEffect(() => {
+    if (count >= 1) {
+      document.title = `count ${count}`
+    } else {
+      document.title = `count`
+    }
+    console.log("hi useffect")
+  })
+  console.log("Hello")
 
-    useEffect(() => {
-        if (count>=1){
-            document.title= `count ${count}`
-        }else{
-            document.title = `count`
-        }
-        console.log("hi useffect")
-    })
-    console.log("Hello")
+  // useEffect(() => {
+  //   if (count >= 1) {
+  //     document.title = `count ${count}`
+  //   } else {
+  //     document.title = `count`
+  //   }
+  //   console.log("hi useffect")
+  // }, [])
+
+  // useEffect(() => {
+  //   if (count >= 1) {
+  //     document.title = `count ${count}`
+  //   } else {
+  //     document.title = `count`
+  //   }
+  //   console.log("hi useffect")
+  // }, [count])
 
   return (
     <div><h1>{count}</h1>
-    <button onClick={()=>setcount(count+1)}>Click</button>
+      <button onClick={() => setcount(count + 1)}>Click</button>
     </div>
   )
 }
